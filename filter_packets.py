41082ffce5c9f43d8056274879ba9c5e2ed6f689
vww9966 <vww9966@rit.edu>
2021-05-03 21:35:33 -0400

    Filter packet completed, small changes to usability necessary.

--- filter_packets.py
+++ filter_packets.py
@@ -2,5 +2,23 @@
 # filter_packets
 # Specs: Filter node packets based off of the type
 
-def filter() :
+
+file="Node1.txt"
+writefile="Node1_ICMP.txt"
+def filter():
 	print('called filter function in filter_packets.py')
+	readfile(file)
+
+def readfile(filename):
+	f = open(filename)
+	fw = open(writefile, 'a')
+	line = f.readline()
+	while line:
+		line = f.readline()
+		if("ICMP" in line):
+			fw.write(line)
+	
+	f.close()
+	fw.close()
+
+filter()
