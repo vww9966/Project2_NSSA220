cc52f342ce74503e5e7ca9f69f3fc0f33c41c5dd
vww9966 <vww9966@rit.edu>
2021-05-04 23:19:57 -0400

    Not much changes but added some stuff to compute metrics showing what data to collect

--- compute_metrics.py
+++ compute_metrics.py
@@ -4,13 +4,37 @@
 
 from Packet import Packet
 
+# --- Results key ---
+#	Num. requests sent
+#	Num. request received
+#	Num. replies sent
+#	Num. replies received
+#	Total frame echo bytes sent
+#	Total frame echo bytes received
+#	Total echo data bytes sent
+#	Total echo data bytes received
+#	Average ping round trip time
+#	Echo request throughput kB/s
+#	Echo request goodput
+#	Average reply delay
+#	Average num. of hops per echo request
+
+
 # parsed_packets - a list of all the parsed packets to be computed
 def compute(parsed_packets) :
 	print('called compute function in compute_metrics.py')
 
+	
 	# List of results to be returned
-	results = []
+	results = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+
+	for packet in parsed_packets:
+		if 'reply' in packet.packet_type:
+			results[0]+=1
+	
 	# return the results
 	return results
+
+
 
