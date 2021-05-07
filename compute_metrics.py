320cb58f362cd0ea2b9110cfe5fae7ac8c72e556
Rick Wallert <rdw1555@rit.edu>
2021-05-06 22:01:31 -0400

    Cleaned up the computing function

--- compute_metrics.py
+++ compute_metrics.py
@@ -5,27 +5,30 @@
 from Packet import Packet
 
 # --- Results key ---
-#	0  Num. requests sent
-#	1  Num. request received
-#	2  Num. replies sent
-#	3  Num. replies received
-#	4  Total frame echo bytes sent
-#	5  Total frame echo bytes received
-#	6  Total echo data bytes sent
-#	7  Total echo data bytes received
-#	8  Average ping round trip time
-#	9  Echo request throughput kB/s
-#	10 Echo request goodput
-#	11 Average reply delay
-#	12 Average num. of hops per echo request
+# 0  Num. requests sent
+# 1  Num. request received
+# 2  Num. replies sent
+# 3  Num. replies received
+# 4  Total frame echo bytes sent
+# 5  Total frame echo bytes received
+# 6  Total echo data bytes sent
+# 7  Total echo data bytes received
+# 8  Average ping round trip time
+# 9  Echo request throughput kB/s
+# 10 Echo request goodput
+# 11 Average reply delay
+# 12 Average num. of hops per echo request
 
+# Store all of the possible node ip's
 nodeip=["192.168.100.1","192.168.100.2","192.168.200.1","192.168.200.2"]
 
 # parsed_packets - a list of all the parsed packets to be computed
+# node - the current node number (1,2,3, or 4)
 def compute(parsed_packets,node) :
 	# List of results to be returned
 	results = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+	# Helper variables for calculations
 	rttsum=0
 	replydelay=0
 	rtttotal=0
@@ -34,9 +37,9 @@ def compute(parsed_packets,node) :
 	totalpackets=0
 	previous_packet=None
 
+	# Loop through every parsed packet object
 	for packet in parsed_packets:
-
-		#Requests sent/received
+		# Calculate Requests sent/received
 		if 'request' in packet.packet_type:
 			if nodeip[node-1]==packet.source:
 				results[0]+=1
@@ -48,19 +51,19 @@ def compute(parsed_packets,node) :
 			else:
 				results[3]+=1
 		
-		#Frame and payload bytes
+		# Calculate Frame and payload bytes
 		if nodeip[node-1]==packet.source:
-			#Frame
+			# Frame
 			results[4]+=int(packet.packet_length)
-			#Payload
+			# Payload
 			results[6]+=(int(packet.packet_length)-20)
 		else:
-			#Frame
+			# Frame
 			results[5]+=int(packet.packet_length)
-			#Payload
+			# Payload
 			results[7]+=(int(packet.packet_length)-20)
 
-		#Ping RTT + Ping reply delay
+		# Calculate Ping RTT + Ping reply delay
 		if previous_packet is not None and packet.seq in previous_packet.seq:
 			if packet.source==nodeip[node-1]:
 				rttsum+=(float(packet.time)-float(previous_packet.time))
@@ -69,7 +72,7 @@ def compute(parsed_packets,node) :
 				replydelay+=(float(packet.time)-float(previous_packet.time))
 				replydelaytotal+=1
 
-		#Average hops count
+		# Calculate Average hops count
 		if packet.ttl != '128':
 			hopcountsum+=3
 		else:
@@ -78,21 +81,20 @@ def compute(parsed_packets,node) :
 
 		previous_packet=packet
 
-	#Average RTT
+	# Calculate Average RTT
 	results[8]=(rttsum/rtttotal)*1000
 	
-	#Echo request throughput
+	# Calculate Echo request throughput
 	results[9]=float((results[4]/1000)/rttsum)
 
-	#Echo request goodput
+	# Calculate Echo request goodput
 	results[10]=float((results[6]/1000)/rttsum)	
 
-	#Reply delay
+	# Calculate Reply delay
 	results[11]=(replydelay/replydelaytotal)*1000000
 
-	#Average number of hops per echo request
-	#print(hopcountsum)
-	#print(totalpackets)
+	# Calculate Average number of hops per echo request
 	results[12]=float(hopcountsum/totalpackets)
-	# return the results
+
+	# return this list of results
 	return results
